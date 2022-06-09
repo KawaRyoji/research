78dfadbc7307e008cbf0e3d73dfcab65d132af1e
kawa <180441035@ccalumni.meijo-u.ac.jp>
2022-06-09 16:41:31 +0900

    added window length comparison results

--- experiments/da_net/da_compare.py
+++ experiments/da_net/da_compare.py
@@ -10,7 +10,6 @@ waveform_test_poly = learning_history.from_path(
     "./experimental_results/da_waveform_poly/result_5fold/test_res.csv"
 )
 
-
 # 時間波形　単音高推定
 waveform_mono = learning_history.from_dir(
     "./experimental_results/da_waveform_mono/result_5fold/histories"
@@ -19,7 +18,6 @@ waveform_mono = learning_history.average(*waveform_mono)
 waveform_test_mono = learning_history.from_path(
     "./experimental_results/da_waveform_mono/result_5fold/test_res.csv"
 )
-
 
 # スペクトル　複数音高推定
 spec_poly = learning_history.from_dir(
@@ -30,7 +28,6 @@ spec_test_poly = learning_history.from_path(
     "./experimental_results/da_spec_poly/result_5fold/test_res.csv"
 )
 
-
 # スペクトル　単音高推定
 spec_mono = learning_history.from_dir(
     "./experimental_results/da_spec_mono/result_5fold/histories"
@@ -40,8 +37,7 @@ spec_test_mono = learning_history.from_path(
     "./experimental_results/da_spec_mono/result_5fold/test_res.csv"
 )
 
-
-# スペクトル(標準化なし)　複数音高推定
+# スペクトル(標準化なし)　複数音高推定 flen=1024
 spec_not_normalized_poly = learning_history.from_dir(
     "./experimental_results/da_spec_not_normalized_poly/result_5fold/histories"
 )
@@ -49,7 +45,6 @@ spec_not_normalized_poly = learning_history.average(*spec_not_normalized_poly)
 spec_not_normalized_test_poly = learning_history.from_path(
     "./experimental_results/da_spec_not_normalized_poly/result_5fold/test_res.csv"
 )
-
 
 # 対数スペクトル　複数音高推定
 logspec_poly = learning_history.from_dir(
@@ -60,7 +55,6 @@ logspec_test_poly = learning_history.from_path(
     "./experimental_results/da_logspec_poly/result_5fold/test_res.csv"
 )
 
-
 # 対数スペクトル　単音高推定
 logspec_mono = learning_history.from_dir(
     "./experimental_results/da_logspec_mono/result_5fold/histories"
@@ -70,6 +64,32 @@ logspec_test_mono = learning_history.from_path(
     "./experimental_results/da_logspec_mono/result_5fold/test_res.csv"
 )
 
+# スペクトル(標準化なし) 複数音高推定 flen=2048
+spec_w2048_poly = learning_history.from_dir(
+    "./experimental_results/da_spec_w2048_poly/result_5fold/histories"
+)
+spec_w2048_poly = learning_history.average(*spec_w2048_poly)
+spec_w2048_test_poly = learning_history.from_path(
+    "./experimental_results/da_spec_w2048_poly/result_5fold/test_res.csv"
+)
+
+# スペクトル(標準化なし) 複数音高推定 flen=4096
+spec_w4096_poly = learning_history.from_dir(
+    "./experimental_results/da_spec_w4096_poly/result_5fold/histories"
+)
+spec_w4096_poly = learning_history.average(*spec_w4096_poly)
+spec_w4096_test_poly = learning_history.from_path(
+    "./experimental_results/da_spec_w4096_poly/result_5fold/test_res.csv"
+)
+
+# スペクトル(標準化なし) 複数音高推定 flen=512
+spec_w512_poly = learning_history.from_dir(
+    "./experimental_results/da_spec_w512_poly/result_5fold/histories"
+)
+spec_w512_poly = learning_history.average(*spec_w512_poly)
+spec_w512_test_poly = learning_history.from_path(
+    "./experimental_results/da_spec_w512_poly/result_5fold/test_res.csv"
+)
 
 metrics = ["precision", "recall", "F1"]
 
@@ -119,7 +139,7 @@ cplot.plot_histories(
     spec_poly,
     spec_not_normalized_poly,
     metrics=metrics,
-    legend=["normalized", "not normalized"]
+    legend=["normalized", "not normalized"],
 )
 
 cplot.box_plot_histories(
@@ -127,5 +147,26 @@ cplot.box_plot_histories(
     spec_test_poly,
     spec_not_normalized_test_poly,
     metrics=metrics,
-    legend=["normalized", "not normalized"]
+    legend=["normalized", "not normalized"],
+)
+
+# フレーム長による比較
+cplot.plot_histories(
+    "./comparison_results/da_window_len_poly",
+    spec_w512_poly,
+    spec_not_normalized_poly,
+    spec_w2048_poly,
+    spec_w4096_poly,
+    metrics=metrics,
+    legend=["512", "1024", "2048", "4096"],
+)
+
+cplot.box_plot_histories(
+    "./comparison_results/da_window_len_poly/test_res_compare.png",
+    spec_w512_test_poly,
+    spec_not_normalized_test_poly,
+    spec_w2048_test_poly,
+    spec_w4096_test_poly,
+    metrics=metrics,
+    legend=["512", "1024", "2048", "4096"],
 )
